1291743dea7ede067ca2fc80323ffe7c1959e95a
Francisc Munteanu <francisc.munteanu@ditechspa.it>
2016-10-24 10:53:54 +0200

    10038 solution

--- 10038-JollyJumpers/a.cpp
+++ 10038-JollyJumpers/a.cpp
@@ -1,35 +1,54 @@
 #include<iostream>
 #include <cmath>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-    while(true) {
-        int n = -1;
-        cin>>n; 
-        if(n == -1) break;
+	while(true) {
+		int n = -1;
+		cin>>n; 
+		if(n == -1) break;
 
-        int diff = 0;
-        int temp = 0;
-        int x;
-        string res = "Jolly";
-        for(int i = 0; i < n; i++) {
-            cin>>x; 
-            temp = abs(temp - x);
-            cout << diff << endl;
-            if(diff == 0){
-                diff = temp; 
-            }else {
-                if(diff != abs(temp + 1)) {
-                    res = "Not jolly"; 
-                }
-                temp = diff;
-            }
+		int x;
+		int nums[3010];
+		int diffs[3010];
 
-        }
-        cout << res << endl;
-    }
+		for(int i = 0; i < n; i++) {
+			cin>>x; 
+			nums[i] = x;
 
-    return 0;
+		}
+
+		int i;
+		for(i = 0; i < n-1; i++){
+			diffs[i] = abs(nums[i+1] - nums[i]);
+		}
+
+		sort(diffs, diffs+i);	
+
+
+		int j = 1;
+		for(int i = 1; i < n; i++){
+
+			if(diffs[i] == diffs[i-1]){
+				j = 0;
+				break;
+			}
+
+		}
+
+		if(j == 1){
+			cout << "Jolly";
+		}else{
+			cout << "Not jolly";
+		}
+		cout << endl;
+
+
+	}
+
+	return 0;
 }
