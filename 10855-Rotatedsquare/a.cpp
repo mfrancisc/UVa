acdd16771b294f1ef222f3d2367366a4a3595afa
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-10-24 23:09:10 +0200

    10855 solution

--- 10855-Rotatedsquare/a.cpp
+++ 10855-Rotatedsquare/a.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
@@ -8,74 +9,91 @@ char nsq[1000][1000];
 int N,n;
 int verify(){
 
-	int cntCol, cntRow;
-	int found = 0;
-	for(int row = 0; row < N; row++){
-		for(int col = 0; col < N; col++){
+    int found = 0;
+    for(int row = 0; row < N; row++){
+        for(int col = 0; col < N; col++){
 
-			cntRow = 0;
-			for(int i = 0; i < n; i++){
-				cntCol = 0;
-				for(int e = 0; e < n; e++){
+            int cnt = 0;
+            for(int i = 0; i < n; i++){
+                for(int e = 0; e < n; e++){
 
-					char nC = nsq[i][e];
-					char NC = Nsq[row+i][col+e];
-					if(nC == NC) cntCol++;
+                    char nC = nsq[i][e];
+                    char NC = Nsq[row+i][col+e];
+                    if(nC == NC) cnt++;
 
-				}
+                }
+            }
 
-				if(cntCol==n){ cntRow++;}
-			}
+            if(cnt==n*n) {
+                found++;
+            }
 
-			if(cntRow==n) {
-				found = 1;
-				break;
-			}
+        }
+    }
 
-		}
-	}
-
-	return found;
+    return found;
 
 }
 
 int main() {
-	while(true){
+    while(true){
+        memset(nsq, ' ', sizeof(nsq));
+        memset(Nsq, ' ', sizeof(Nsq));
+        cin>>N>>n;
 
-		cin>>N>>n;
-
-		if(N==0 && n == 0){
-			break;
-		}
-
-		// input squares
-		for(int i = 0; i < N; i++){
-			string s;
-			cin>>s;
-
-			for(int e = 0; e < N; e++){
-				Nsq[i][e] = s[e];
-			}
-		}
-		for(int i = 0; i < n; i++){
-			string s;
-			cin>>s;
-			for(int e = 0; e < n; e++){
-				nsq[i][e] = s[e];
-			}
-		}
-
-		// verify O°
-		int found = verify();
-		cout << found << endl;
+        if(N==0 && n == 0){
+            break;
+        }
 
 
-		// verify 90°
-	
+        // input squares
+        for(int i = 0; i < N; i++){
+            string s;
+            cin>>s;
 
-	}
+            for(int e = 0; e < N; e++){
+                Nsq[i][e] = s[e];
+            }
+        }
+        for(int i = 0; i < n; i++){
+            string s;
+            cin>>s;
+            for(int e = 0; e < n; e++){
+                nsq[i][e] = s[e];
+            }
+        }
+        // verify O°
+        int found = verify();
+        cout << found << " ";
+
+        // 3 rotationr
+        // verify O° --> 90° --> 180° --> 270°
+        for(int x = 0; x < 3; x++) {
+            // save matrix
+            char newNsq[n][n];
+
+            // verify 90°
+            for(int e = 0; e < n; e++) {
+                for(int w = 0; w < n; w++) {
+                    newNsq[e][w] = nsq[n-w-1][e];
+                }
+            }
+            for(int e = 0; e < n; e++) {
+                for(int w = 0; w < n; w++) {
+                    nsq[e][w] = newNsq[e][w];
+                }
+            }
+            found = verify();
+            cout << found;
+            if(x != 2){
+                cout << " "; 
+            }
+        }
+        cout << endl;
+
+    }
 
 
 
-	return 0;
+    return 0;
 }
